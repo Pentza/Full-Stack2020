0158eb857bd6d874bc8bb0f331ea6d0599cc8222
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-11-27 16:55:55 +0200

    part3

--- osa-2/puhelinluettelo/src/components/App.js
+++ osa-2/puhelinluettelo/src/components/App.js
@@ -56,6 +56,10 @@ const App = () => {
               setNewNumber('')
               setErrMessage(`${newName} added`, false)
           })
+          .catch(error => {
+            console.log(error)
+            setErrMessage(`${error.response.data.error}`, true)
+          })
     }
   }
   
