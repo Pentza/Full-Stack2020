ba5fa5cf9a5df6deb14a66af435fc0a9d7161f0a
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-11-29 15:44:39 +0200

    add logger module

--- osa-4/blogilista/index.js
+++ osa-4/blogilista/index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
+const config = require('./utils/logger')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -16,7 +18,7 @@ const Blog = mongoose.model('Blog', blogSchema)
 
 //const mongoUrl = 'mongodb+srv://pentsa:qmXviaS596aoEFzt@phonebook.ltm1u.mongodb.net/BlogDB?retryWrites=true&w=majority'
 const mongoUrl = process.env.MONGO_URL
-console.log(mongoUrl)
+
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
@@ -42,5 +44,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
